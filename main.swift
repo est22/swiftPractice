0f97e6975ad764d2c52f1c5c8f07b2d9c3dc5ad5
est22 <60957334+est22@users.noreply.github.com>
2021-09-06 11:39:07 +0000

    변수와 상수, Packaging

--- main.swift
+++ main.swift
@@ -4,8 +4,8 @@ Practice1. SignUser
 
 var nickname: String = "고래밥"
 var age: Int = 33
-let emain: String = "mono@monocoding.com"   // 변하지 않는 상수
+let email: String = "mono@monocoding.com"   // 변하지 않는 상수
 var rate: Double = 4.5
 var gender: Bool = true
 
-print("안녕하세요")
+print("안녕하세요 \(nickname)입니다. 저는 \(age)살 이고, 이메일은 \(email)입니다. 평점은 \(rate)에요.")   // 블럭 처리 후 괄호(Packaging)
