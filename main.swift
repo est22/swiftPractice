1e57de5a44087fffb25f402f73fc0cba1d404b40
est22 <60957334+est22@users.noreply.github.com>
2021-09-06 11:44:39 +0000

    자료형 명시, 변수와 상수의 차이

--- main.swift
+++ main.swift
@@ -2,10 +2,20 @@
 Practice1. SignUser
 */
 
-var nickname: String = "고래밥"
-var age: Int = 33
-let email: String = "mono@monocoding.com"   // 변하지 않는 상수
-var rate: Double = 4.5
-var gender: Bool = true
+// 변수 선언
+var nickname = "고래밥"
+var age = 33
+let email = "mono@monocoding.com"   // 변하지 않는 상수
+var rate: Double = 4 // 밑의 line 17에서 에러가 나므로 4.0으로 작성하거나 자료형 명시
+var gender = true
 
 print("안녕하세요 \(nickname)입니다. 저는 \(age)살 이고, 이메일은 \(email)입니다. 평점은 \(rate)에요.")   // 블럭 처리 후 괄호(Packaging)
+
+// 6개월 지나 회원 정보 수정
+nickname = "칙촉"
+age = 20
+rate = 3.3
+gender = false
+// email = "test@test.com" -> let으로 선언한 상수는 변경불가.
+
+print("안녕하세요 \(nickname)입니다. 저는 \(age)살 이고, 이메일은 \(email)입니다. 평점은 \(rate)에요.") 
