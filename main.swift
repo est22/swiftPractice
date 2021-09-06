a3395b935b26e9b74958608ebf6c8ca59d102b0a
est22 <60957334+est22@users.noreply.github.com>
2021-09-06 11:35:54 +0000

    Initial commit

---
+++ main.swift
@@ -0,0 +1,11 @@
+/*
+Practice1. SignUser
+*/
+
+var nickname: String = "고래밥"
+var age: Int = 33
+let emain: String = "mono@monocoding.com"   // 변하지 않는 상수
+var rate: Double = 4.5
+var gender: Bool = true
+
+print("안녕하세요")
