4404f78d1f2bb0bcffffefface48078363e18d06
est22 <anlia.seoul@gmail.com>
2021-10-06 18:15:20 +0900

    upload all

---
+++ SeSACEmotionDiary/SeSACEmotionDiary/SeSACEmotionDiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeSACEmotionDiaryApp.swift
+//  SeSACEmotionDiary
+//
+//  Created by Lia AN on 2021/10/05.
+//
+
+import SwiftUI
+
+@main
+struct SeSACEmotionDiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
