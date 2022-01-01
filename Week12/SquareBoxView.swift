0c888135d6ae347f921abca7794ba6fa9452cfb2
est22 <60957334+est22@users.noreply.github.com>
2022-01-01 23:58:52 +0900

    XIB

---
+++ Week12/SquareBoxView.swift
@@ -0,0 +1,14 @@
+//
+//  SquareBoxView.swift
+//  Week12
+//
+//  Created by Lia AN on 2022/01/01.
+//
+
+import UIKit
+
+class SquareBoxView: UIView {
+    
+    
+    
+}
---
+++ Week12/Week12/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Week12
+//
+//  Created by Lia AN on 2022/01/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
